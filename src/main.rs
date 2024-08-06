293563792f2df5bec900ac140a65729ae310c0cf
Matilde Morrone <contact@morrone.dev>
2024-08-06 20:24:05 +0200

    Publish grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("O////w////O");
+use anyhow::Result;
+use clap::Parser;
+use pest::Parser as _;
+use std::{fs, path::PathBuf};
+
+mod parser;
+// mod jit;
+
+use parser::{PennyParser, Rule};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg()]
+    source: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let Args { source } = Args::parse();
+    let source = fs::read_to_string(source)?;
+
+    let pairs = PennyParser::parse(Rule::program, &source)?;
+    println!("parse tree = {:#?}", pairs);
+
+    for pair in pairs {
+        match pair.as_rule() {
+            rule => {
+                dbg!(rule);
+            }
+        }
+    }
+
+    Ok(())
 }
